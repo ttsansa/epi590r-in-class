5eb4eb6bab7390bb50b08e9a6af03d53145d268f
Louisa Smith <louisahsmith@gmail.com>
2024-08-19 21:11:04 -0400

    fix weird indentation

--- R/regression-examples.R
+++ R/regression-examples.R
@@ -2,17 +2,22 @@ library(tidyverse)
 library(gtsummary)
 
 # load and clean data
-nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
-							 "id", "nsibs", "samp", "race_eth", "sex", "region",
-							 "income", "res_1980", "res_2002", "age_bir")
+nlsy_cols <- c(
+  "glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
+  "id", "nsibs", "samp", "race_eth", "sex", "region",
+  "income", "res_1980", "res_2002", "age_bir"
+)
 nlsy <- read_csv(here::here("data", "raw", "nlsy.csv"),
-								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
-								 skip = 1, col_names = nlsy_cols) |>
-	mutate(region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
-				 sex_cat = factor(sex, labels = c("Male", "Female")),
-				 race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
-				 eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
-				 glasses_cat = factor(glasses, labels = c("No", "Yes")))
+  na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+  skip = 1, col_names = nlsy_cols
+) |>
+  mutate(
+    region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
+    sex_cat = factor(sex, labels = c("Male", "Female")),
+    race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
+    eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
+    glasses_cat = factor(glasses, labels = c("No", "Yes"))
+  )
 
 
 # Univariate regression
@@ -20,11 +25,14 @@ nlsy <- read_csv(here::here("data", "raw", "nlsy.csv"),
 
 # regression of income on a series of predictor (x) variables
 tbl_uvregression(
-	nlsy,
-	y = income,
-	include = c(sex_cat, race_eth_cat,
-							eyesight_cat, income, age_bir),
-	method = lm)
+  nlsy,
+  y = income,
+  include = c(
+    sex_cat, race_eth_cat,
+    eyesight_cat, income, age_bir
+  ),
+  method = lm
+)
 ## this is equivalent to
 # lm(income ~ sex_cat, data = nlsy)
 # lm(income ~ race_eth_cat, data = nlsy)
@@ -36,13 +44,16 @@ tbl_uvregression(
 # regression of glasses on a series of predictor (x) variables
 # using a logistic regression model
 tbl_uvregression(
-	nlsy,
-	y = glasses,
-	include = c(sex_cat, race_eth_cat,
-							eyesight_cat, glasses, age_bir),
-	method = glm,
-	method.args = list(family = binomial()),
-	exponentiate = TRUE)
+  nlsy,
+  y = glasses,
+  include = c(
+    sex_cat, race_eth_cat,
+    eyesight_cat, glasses, age_bir
+  ),
+  method = glm,
+  method.args = list(family = binomial()),
+  exponentiate = TRUE
+)
 
 
 ## Multivariable regressions
@@ -50,39 +61,44 @@ tbl_uvregression(
 
 # linear model
 linear_model <- lm(income ~ sex_cat + age_bir + race_eth_cat,
-									 data = nlsy)
+  data = nlsy
+)
 
 # linear model with interaction
-linear_model_int <- lm(income ~ sex_cat*age_bir + race_eth_cat,
-											 data = nlsy)
+linear_model_int <- lm(income ~ sex_cat * age_bir + race_eth_cat,
+  data = nlsy
+)
 
 # logistic model
 logistic_model <- glm(glasses ~ eyesight_cat + sex_cat + income,
-											data = nlsy, family = binomial())
+  data = nlsy, family = binomial()
+)
 
 
 ## Tables
 # we use the models we just fit to create the tables
 tbl_regression(
-	linear_model,
-	# include the intercept
-	intercept = TRUE,
-	# relabel the variables
-	label = list(
-		sex_cat ~ "Sex",
-		race_eth_cat ~ "Race/ethnicity",
-		age_bir ~ "Age at first birth"
-	))
+  linear_model,
+  # include the intercept
+  intercept = TRUE,
+  # relabel the variables
+  label = list(
+    sex_cat ~ "Sex",
+    race_eth_cat ~ "Race/ethnicity",
+    age_bir ~ "Age at first birth"
+  )
+)
 
 
 tbl_regression(
-	logistic_model,
-	exponentiate = TRUE,
-	label = list(
-		sex_cat ~ "Sex",
-		eyesight_cat ~ "Eyesight",
-		income ~ "Income"
-	))
+  logistic_model,
+  exponentiate = TRUE,
+  label = list(
+    sex_cat ~ "Sex",
+    eyesight_cat ~ "Eyesight",
+    income ~ "Income"
+  )
+)
 
 
 # in order to compare the models with and without interaction
@@ -90,28 +106,31 @@ tbl_regression(
 
 # table for the model without interaction
 tbl_no_int <- tbl_regression(
-	linear_model,
-	intercept = TRUE,
-	label = list(
-		sex_cat ~ "Sex",
-		race_eth_cat ~ "Race/ethnicity",
-		age_bir ~ "Age at first birth"
-	))
+  linear_model,
+  intercept = TRUE,
+  label = list(
+    sex_cat ~ "Sex",
+    race_eth_cat ~ "Race/ethnicity",
+    age_bir ~ "Age at first birth"
+  )
+)
 
 # table for the model with interaction
 tbl_int <- tbl_regression(
-	linear_model_int,
-	intercept = TRUE,
-	label = list(
-		sex_cat ~ "Sex",
-		race_eth_cat ~ "Race/ethnicity",
-		age_bir ~ "Age at first birth",
-		`sex_cat:age_bir` ~ "Sex/age interaction"
-	))
+  linear_model_int,
+  intercept = TRUE,
+  label = list(
+    sex_cat ~ "Sex",
+    race_eth_cat ~ "Race/ethnicity",
+    age_bir ~ "Age at first birth",
+    `sex_cat:age_bir` ~ "Sex/age interaction"
+  )
+)
 
 ## Table comparing the models with and without interaction
 tbl_merge(list(tbl_no_int, tbl_int),
-					tab_spanner = c("**Model 1**", "**Model 2**"))
+  tab_spanner = c("**Model 1**", "**Model 2**")
+)
 
 
 #### Exercises ####
@@ -120,52 +139,62 @@ tbl_merge(list(tbl_no_int, tbl_int),
 # now we are using the tbl_uvregression function to fit the models
 # for a single predictor (x) variable and varying the y variable
 tbl_uvregression(
-	nlsy,
-	x = sex_cat,
-	include = c(nsibs, sleep_wkdy,
-							sleep_wknd, income),
-	method = lm)
+  nlsy,
+  x = sex_cat,
+  include = c(
+    nsibs, sleep_wkdy,
+    sleep_wknd, income
+  ),
+  method = lm
+)
 
 # 4
 # again we have to create the model first as its own object
 poisson_model <- glm(nsibs ~ eyesight_cat + sex_cat + income,
-										 data = nlsy, family = poisson())
+  data = nlsy, family = poisson()
+)
 
 # and then we can create the table
 tbl_regression(
-	poisson_model,
-	exponentiate = TRUE,
-	label = list(
-		sex_cat ~ "Sex",
-		eyesight_cat ~ "Eyesight",
-		income ~ "Income"
-	))
+  poisson_model,
+  exponentiate = TRUE,
+  label = list(
+    sex_cat ~ "Sex",
+    eyesight_cat ~ "Eyesight",
+    income ~ "Income"
+  )
+)
 
 # 5
 # create the log-binomial model
 eyes_binomial_model <- glm(glasses ~ eyesight_cat + sex_cat,
-													 data = nlsy, family = binomial(link = "log"))
+  data = nlsy, family = binomial(link = "log")
+)
 
 # turn it into a table
 # we'll store the table as an object so we can use it later
 eyes_binomial_table <- tbl_regression(eyes_binomial_model,
-																			exponentiate = TRUE)
+  exponentiate = TRUE
+)
 # look at the table
 eyes_binomial_table
 
 # 6
 # create the Poisson model
 eyes_poisson_model <- glm(glasses ~ eyesight_cat + sex_cat,
-													data = nlsy, family = poisson(link = "log"))
+  data = nlsy, family = poisson(link = "log")
+)
 
 # turn it into a table
 eyes_poisson_table <- tbl_regression(eyes_poisson_model,
-																		 exponentiate = TRUE,
-																		 tidy_fun = partial(tidy_robust, vcov = "HC1"))
+  exponentiate = TRUE,
+  tidy_fun = partial(tidy_robust, vcov = "HC1")
+)
 # look at the table
 eyes_poisson_table
 
 # 7
 # merge the two tables to compare
 tbl_merge(list(eyes_binomial_table, eyes_poisson_table),
-					tab_spanner = c("**Binomial**", "**Poisson**"))
+  tab_spanner = c("**Binomial**", "**Poisson**")
+)
--- R/table1-examples.R
+++ R/table1-examples.R
@@ -2,101 +2,115 @@ library(tidyverse)
 library(gtsummary)
 
 # Load and clean data
-nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
-							 "id", "nsibs", "samp", "race_eth", "sex", "region",
-							 "income", "res_1980", "res_2002", "age_bir")
+nlsy_cols <- c(
+  "glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
+  "id", "nsibs", "samp", "race_eth", "sex", "region",
+  "income", "res_1980", "res_2002", "age_bir"
+)
 nlsy <- read_csv(here::here("data", "raw", "nlsy.csv"),
-								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
-								 skip = 1, col_names = nlsy_cols) |>
-	mutate(region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
-				 sex_cat = factor(sex, labels = c("Male", "Female")),
-				 race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
-				 eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
-				 glasses_cat = factor(glasses, labels = c("No", "Yes")))
+  na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+  skip = 1, col_names = nlsy_cols
+) |>
+  mutate(
+    region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
+    sex_cat = factor(sex, labels = c("Male", "Female")),
+    race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
+    eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
+    glasses_cat = factor(glasses, labels = c("No", "Yes"))
+  )
 
 
 # simple table
 tbl_summary(
-	nlsy,
-	by = sex_cat,
-	include = c(sex_cat, race_eth_cat, region_cat,
-							eyesight_cat, glasses, age_bir))
+  nlsy,
+  by = sex_cat,
+  include = c(
+    sex_cat, race_eth_cat, region_cat,
+    eyesight_cat, glasses, age_bir
+  )
+)
 
 # add labels for the variables and for the "missing" category
 tbl_summary(
-	nlsy,
-	by = sex_cat,
-	include = c(sex_cat, race_eth_cat, region_cat,
-							eyesight_cat, glasses, age_bir),
-	label = list(
-		race_eth_cat ~ "Race/ethnicity",
-		region_cat ~ "Region",
-		eyesight_cat ~ "Eyesight",
-		glasses ~ "Wears glasses",
-		age_bir ~ "Age at first birth"
-	),
-	missing_text = "Missing")
+  nlsy,
+  by = sex_cat,
+  include = c(
+    sex_cat, race_eth_cat, region_cat,
+    eyesight_cat, glasses, age_bir
+  ),
+  label = list(
+    race_eth_cat ~ "Race/ethnicity",
+    region_cat ~ "Region",
+    eyesight_cat ~ "Eyesight",
+    glasses ~ "Wears glasses",
+    age_bir ~ "Age at first birth"
+  ),
+  missing_text = "Missing"
+)
 
 # add p-values, a total column, bold labels, and remove the footnote
 tbl_summary(
-	nlsy,
-	by = sex_cat,
-	include = c(sex_cat, race_eth_cat,
-							eyesight_cat, glasses, age_bir),
-	label = list(
-		race_eth_cat ~ "Race/ethnicity",
-		eyesight_cat ~ "Eyesight",
-		glasses ~ "Wears glasses",
-		age_bir ~ "Age at first birth"
-	),
-	missing_text = "Missing") |>
-	# change the test used to compare sex_cat groups
-	add_p(test = list(all_continuous() ~ "t.test",
-	                  all_categorical() ~ "chisq.test")) |>
-	# add a total column with the number of observations
-	add_overall(col_label = "**Total** N = {N}") |>
-	bold_labels() |>
-	# remove the default footnotes
-	modify_footnote(update = everything() ~ NA) |>
-	# replace the column headers and make them bold
-	modify_header(label = "**Variable**", p.value = "**P**")
+  nlsy,
+  by = sex_cat,
+  include = c(
+    sex_cat, race_eth_cat,
+    eyesight_cat, glasses, age_bir
+  ),
+  label = list(
+    race_eth_cat ~ "Race/ethnicity",
+    eyesight_cat ~ "Eyesight",
+    glasses ~ "Wears glasses",
+    age_bir ~ "Age at first birth"
+  ),
+  missing_text = "Missing"
+) |>
+  # change the test used to compare sex_cat groups
+  add_p(test = list(
+    all_continuous() ~ "t.test",
+    all_categorical() ~ "chisq.test"
+  )) |>
+  # add a total column with the number of observations
+  add_overall(col_label = "**Total** N = {N}") |>
+  bold_labels() |>
+  # remove the default footnotes
+  modify_footnote(update = everything() ~ NA) |>
+  # replace the column headers and make them bold
+  modify_header(label = "**Variable**", p.value = "**P**")
 
 
 #### Exercises ####
 
 tbl_summary(nlsy,
-						by = sex_cat,
-						include = c(sex_cat, region_cat,
-												race_eth_cat, income,
-												# helper function to select columns that start with "sleep"
-												starts_with("sleep")),
-						label = list(
-							race_eth_cat ~ "Race/ethnicity",
-							region_cat ~ "Region",
-							income ~ "Income",
-							sleep_wkdy ~ "Weekday sleep",
-							sleep_wknd ~ "Weekday sleep"
-						),
-						statistic = list(
-							income ~ "{p10}, {p90}",
-							starts_with("sleep") ~ "{min}, {max}"
-						),
-						digits = list(
-							income ~ c(3, 3), # or income ~ 3
-							starts_with("sleep") ~ c(1, 1) # starts_with("sleep") ~ 1
-						)) |>
-	# p-value and total column
-	add_p() |>
-	add_overall() |>
-	# figured out how to do this from
-	# https://stackoverflow.com/questions/73154658/adding-a-footnote-to-a-single-row-label-in-a-gtsummary-table
-	modify_table_styling(
-		columns = label,
-		rows = label == "Race/ethnicity",
-		footnote = "see https://www.nlsinfo.org/content/cohorts/nlsy79/topical-guide/household/race-ethnicity-immigration-data"
-	)
-
-
-
-
-
+  by = sex_cat,
+  include = c(
+    sex_cat, region_cat,
+    race_eth_cat, income,
+    # helper function to select columns that start with "sleep"
+    starts_with("sleep")
+  ),
+  label = list(
+    race_eth_cat ~ "Race/ethnicity",
+    region_cat ~ "Region",
+    income ~ "Income",
+    sleep_wkdy ~ "Weekday sleep",
+    sleep_wknd ~ "Weekday sleep"
+  ),
+  statistic = list(
+    income ~ "{p10}, {p90}",
+    starts_with("sleep") ~ "{min}, {max}"
+  ),
+  digits = list(
+    income ~ c(3, 3), # or income ~ 3
+    starts_with("sleep") ~ c(1, 1) # starts_with("sleep") ~ 1
+  )
+) |>
+  # p-value and total column
+  add_p() |>
+  add_overall() |>
+  # figured out how to do this from
+  # https://stackoverflow.com/questions/73154658/adding-a-footnote-to-a-single-row-label-in-a-gtsummary-table
+  modify_table_styling(
+    columns = label,
+    rows = label == "Race/ethnicity",
+    footnote = "see https://www.nlsinfo.org/content/cohorts/nlsy79/topical-guide/household/race-ethnicity-immigration-data"
+  )
